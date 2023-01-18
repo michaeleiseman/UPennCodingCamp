eac6c90c2a882edad9dbdba3eef5e019fc831973
michaeleiseman <eiseman@att.net>
2023-01-17 23:09:52 -0500

    add Day 5 Exercises 1 and 2

---
+++ Day05Exercises/Day05_Exercise02_Assignment_Statements/AssignmentStatements/AssignmentStatements.js
@@ -0,0 +1,9 @@
+//Create a variable named hello and set it equal to the element whose id is "howdy"
+
+//Change this element to display UPenn instead of Hello World.
+
+//Give this element a class name of "coolStyle".
+
+//Turn this element invisible
+
+//Make it visible again
