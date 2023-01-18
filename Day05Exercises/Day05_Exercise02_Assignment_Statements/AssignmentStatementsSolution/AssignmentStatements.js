93eb0d7f24cef272d79c925d05562ef328544c6a
michaeleiseman <eiseman@att.net>
2023-01-17 23:25:18 -0500

    improve for Penn branding

--- Day05Exercises/Day05_Exercise02_Assignment_Statements/AssignmentStatementsSolution/AssignmentStatements.js
+++ Day05Exercises/Day05_Exercise02_Assignment_Statements/AssignmentStatementsSolution/AssignmentStatements.js
@@ -1,7 +1,7 @@
 //Create a variable named hello and set it equal to the element whose id is "howdy"
 let hello = document.getElementById("howdy");
 //Change this element to display UPenn instead of Hello World.
-hello.textContent = "UPenn";
+hello.textContent = "Penn";
 //Give this element a class name of "coolStyle".
 hello.className = "coolStyle";
 //Turn this element invisible
