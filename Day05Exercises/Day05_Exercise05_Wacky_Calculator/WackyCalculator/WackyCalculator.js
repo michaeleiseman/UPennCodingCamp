a750db45cce195a146344a4de58ebe234da304b7
michaeleiseman <eiseman@att.net>
2023-01-22 13:04:40 -0500

    style wacky calculator

--- Day05Exercises/Day05_Exercise05_Wacky_Calculator/WackyCalculator/WackyCalculator.js
+++ Day05Exercises/Day05_Exercise05_Wacky_Calculator/WackyCalculator/WackyCalculator.js
@@ -1,7 +1,6 @@
 let firstInput = document.getElementById("firstNumber");
 let secondInput = document.getElementById("secondNumber");
-/*You'll need to attach 'input' event listeners to each of
-these inputs and then define a callback function that adds the value
+/*You'll need to attach a 'click' event listener to the =
+button and define a callback function that adds the value
 in the first input to the value in the second input and
-displays the result in the output span. Note that both inputs
-will be able to use the same callback function in this case.*/
+displays the result in the output span.*/
--- Day05Exercises/Day05_Exercise05_Wacky_Calculator/WackyCalculatorSolution/WackyCalculator.js
+++ Day05Exercises/Day05_Exercise05_Wacky_Calculator/WackyCalculatorSolution/WackyCalculator.js
@@ -1,7 +1,6 @@
 let firstInput = document.getElementById("firstNumber");
 let secondInput = document.getElementById("secondNumber");
-firstInput.addEventListener('input', add, false);
-secondInput.addEventListener('input', add, false);
+document.getElementById("equal").addEventListener('click', add, false);
 function add() {
     document.getElementById("output").textContent = firstInput.value + secondInput.value;
 }
