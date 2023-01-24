f3d2da3dcf5ae71253c8d972f60c3aa51591b83c
michaeleiseman <eiseman@att.net>
2023-01-24 11:08:06 -0500

    add Is It Safe To Drive

---
+++ Day05Exercises/Day05_Exercise08_Is_It_Safe_To_Drive/IsItSafeToDrive/IsItSafeToDrive.js
@@ -0,0 +1,14 @@
+let tempInput = document.getElementById("temp");
+let saltInput = document.getElementById("salt");
+let advisory = document.getElementById("advisory");
+//Notice we are using the input event, so we don't need a "submit" button.
+//The input event fires everytime the value of the input changes.
+tempInput.addEventListener('input', advise, false);
+saltInput.addEventListener('input', advise, false);
+/* This analyzes whether or not it is safe to drive after a snowfall.
+Only if the temperature is greater than 32F or if the temperature
+is greater than 15F and the roads are salted is it safe to drive */
+function advise() {
+    //Your code goes in here.
+    
+}
---
+++ Day05Exercises/Day05_Exercise08_Is_It_Safe_To_Drive/IsItSafeToDriveSolution/IsItSafeToDrive.js
@@ -0,0 +1,22 @@
+let tempInput = document.getElementById("temp");
+let saltInput = document.getElementById("salt");
+let advisory = document.getElementById("advisory");
+//Notice we are using the input event, so we don't need a "submit" button.
+//The input event fires everytime the value of the input changes.
+tempInput.addEventListener('input', advise, false);
+saltInput.addEventListener('input', advise, false);
+/* This analyzes whether or not it is safe to drive after a snowfall.
+Only if the temperature is greater than 32F or if the temperature
+is greater than 15F and the roads are salted is it safe to drive */
+function advise() {
+    let text = " not";
+    let className = "red";
+    let roadsAreSalted = saltInput.value == "yes";
+    let temperature = tempInput.value;
+    if(temperature > 32 || (temperature >= 15 && roadsAreSalted)) {
+        text = "";
+        className = "black"
+    }
+    advisory.textContent = "It is" + text + " safe to drive.";
+    advisory.className = className;
+}
