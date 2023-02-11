24355d45360d333523e12588e5854dc41d0f2fe2
michaeleiseman <eiseman@att.net>
2023-02-11 17:03:09 -0500

    add parameters demo

---
+++ Day07Exercises/Day07_Exercise02_Multiply/Multiply/Multiply.js
@@ -0,0 +1,17 @@
+let button = document.getElementsByTagName("button")[0]
+button.addEventListener('click', report, false);
+//The report function should call the multiply function to get
+//the product of the two numbers in the inputs and then
+//write it to the text content of the output span
+function report() {
+   
+}
+//the multiply function should
+function multiply() {
+    //get the value of the number in the first input element,
+    
+    //get the value of the number in the second input element,
+   
+    //and then RETURN the product of these two numbers back to the report function
+
+}
