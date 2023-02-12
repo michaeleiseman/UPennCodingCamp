0d07ad7f0cc78e9d312865b18aff19cd460634e8
michaeleiseman <eiseman@att.net>
2023-02-12 13:06:59 -0500

    update prime factors

---
+++ Day07Exercises/Day03_Exercise03_PrimeFactors/PrimeFactorsSolution/PrimeFactors.js
@@ -0,0 +1,80 @@
+document.getElementsByTagName("button")[0].addEventListener('click', primeFactorize, false);
+let numberElement = document.getElementsByTagName("input")[0];
+let output = document.getElementById("output");
+function primeFactorize() {
+    let number = Number(numberElement.value);
+    let numberIsComposite = false;
+    let primeNumber = smallestPrimeFactor(number, 2);
+    if(primeNumber) {
+        numberIsComposite = true;
+    }
+    let primeFactors = [];
+    //Keep dividing the number by prime factors
+    //until all that is left is a single prime factor.
+    while(numberIsComposite) {
+        //append the divisor to the prime factors array
+        primeFactors.push(primeNumber);
+        //divide the number by the prime number
+        number = number / primeNumber;
+        //find the next smallest prime factor
+        primeNumber = smallestPrimeFactor(number, primeNumber);
+        //if there was no smallest prime factor except for the number itself
+        if(!primeNumber) {
+            //stop this while loop
+            numberIsComposite = false;
+            //append the number to the prime factors array (it must now be prime)
+            primeFactors.push(number);
+        }
+    }
+    //change the text content of output by calling the getOutputText function
+    //and provide the primeFactors array as a parameter.
+    output.textContent = getOutputText(primeFactors);
+}
+//This function returns the smallest prime factor of number that is greater
+//or equal to the divisor provided. If the number is composite, the divisor
+//provided must be less than or equal to the lowest prime factor of the number
+//if the number is a prime number, it returns false.
+function smallestPrimeFactor(number, divisor) {
+    //we must try to divide our number by divisors from 2 up to the 
+    //square root of the number.
+    //Note that we only want to take the square root ONCE, because
+    //taking square roots can be computationally intensive. We don't
+    //want to have a square root computation in our while condition
+    //that will need to be run repeatedly
+    let max = Math.sqrt(number);
+    while(divisor <= max) {
+        if(number % divisor == 0) {
+            return divisor; 
+        } else {
+            divisor++;
+        }
+    }
+    return false;
+}
+//create a function here called getOutputText that takes an array
+//as a parameter and constructs a sentence that describes what 
+//the prime factors of the number are.
+function getOutputText(primeFactors) {
+    //if there are no prime factors
+    if(primeFactors.length == 0) {
+        //return text indicating this number is a prime number
+        return numberElement.value + " is a prime number.";
+    }
+    //Start the output.
+    let text = "The prime factors of " + numberElement.value + " are ";
+    //Iterate through each prime factor (hint: use an INDEXED loop).
+    //Make a sentence that lists the prime factors of the number.
+    //For example, if the primeFactors array is [2, 3, 7], the output
+    //should be "The prime factors of 42 are 2, 3, and 7."
+    for(let i = 0; i < primeFactors.length; i++) {
+        let factor = primeFactors[i];
+        if(i == primeFactors.length - 1) {
+            text = text + "and " + factor + ".";
+        } else if (i == primeFactors.length - 2 && primeFactors.length == 2){
+            text = text + factor + " ";
+        } else {
+            text = text + factor + ", ";
+        }
+    }
+    return text;
+}
