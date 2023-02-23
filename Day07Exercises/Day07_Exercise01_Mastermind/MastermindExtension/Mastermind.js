b1e52e1c53db29bde1ee7d210b66c3c8d217db1d
michaeleiseman <eiseman@att.net>
2023-02-23 18:07:44 -0500

    add comments to mastermind extension

--- Day07Exercises/Day07_Exercise01_Mastermind/MastermindExtension/Mastermind.js
+++ Day07Exercises/Day07_Exercise01_Mastermind/MastermindExtension/Mastermind.js
@@ -7,6 +7,7 @@ let selects = document.getElementsByTagName("select")
 for(let select of selects) {
     select.addEventListener('change', changeColor, false);
 }
+//change the background color of the select tag to match its value
 function changeColor() {
     let color = this.value;
     this.style.setProperty("background-color", color);
@@ -24,6 +25,7 @@ function reset() {
     //activate the submit button
     submitButton.addEventListener('click', submit, false);
     submitButton.textContent = "Submit Guess";
+    //clear any rows from a previous game if they exist
     for(let i = document.getElementsByClassName("mmRow").length - 1; i > 0; i--) {
         document.getElementById("gameBoard").removeChild(document.getElementsByClassName("mmRow")[i]);
     }
@@ -45,58 +47,71 @@ function submit() {
     for(let select of selects) {
         guess.push(select.value);
     }
+    //determine the feedback to give the user based on their guess
     let feedback = determineFeedback(guess);
+    //append the row containing the guess indicators and the feedback indicators to the game board 
     document.getElementById("gameBoard").appendChild(createNewRow(guess, feedback));
+    //if the feedback array is 4 colors long and there are no "white"s in the array, the user has won, so
     if(feedback.length === 4 && !feedback.some(value => value === "white")) {
+        //deactivate the submit button
         submitButton.removeEventListener("click", submit, false);
+        //let the user know they have won.
         submitButton.textContent = "You won!";
     }
 }
-//this function returns an array containing two numbers.  The first number represents the number of colors in the user's guess that are in the correct place.  The second number represents the number of colors in the user's guess that are in the wrong place
+//this function returns an array containing a series of colors - either "black" or "white". Each "black" string represents a color that the user guessed correctly in the right place.  Each "white" string represents a correctly guessed color, but in the wrong place.
 function determineFeedback(guess) {
     let numberOfCorrectColorsInTheWrongPlace = 0;
     let numberOfCorrectColorsInTheCorrectPlace = 0;
+    let numberOfCorrectColors = 0;
+    let feedback = [];
     //step through each POSITION to determine how many colors are in the correct place
     for(let i = 0; i < 4; i++) {
         if(guess[i] === secretCode[i]) {
             numberOfCorrectColorsInTheCorrectPlace++;
+            feedback.push("black");
         }
     }
     //step through each COLOR to determine how many of the colors are correct (note that this also counts the colors that are in the CORRECT place)
     for(let color of colors) {
         //the number correct for a given color is the lesser of the number of times that color occurs in the answer and the number of times that color occurs in the guess. 
-        numberOfCorrectColorsInTheWrongPlace += Math.min(secretCode.filter(c => c === color).length, guess.filter(c => c === color).length);
+        numberOfCorrectColors += Math.min(secretCode.filter(c => c === color).length, guess.filter(c => c === color).length);
     }
     //The above method counts colors if they were also in the correct place, so we need to subtract out the number of colors in the correct place from the number of correct colors
-    numberOfCorrectColorsInTheWrongPlace -= numberOfCorrectColorsInTheCorrectPlace;
-    //The feedback will be an array of strings - either "black" or "white". Each "black" value will represent a color that is in the correct place.
-    //Each "white" value will represent a color that is in the wrong place.
-    feedback = [];
-    for(let i = 0; i < numberOfCorrectColorsInTheCorrectPlace; i++) {
-        feedback.push("black");
-    }
+    numberOfCorrectColorsInTheWrongPlace = numberOfCorrectColors - numberOfCorrectColorsInTheCorrectPlace;
+    //Now place a "white" in the feedback container for each correct color in the wrong place
     for(let i = 0; i < numberOfCorrectColorsInTheWrongPlace; i++) {
         feedback.push("white");
     }
     return feedback;
 }
+//create the elements indicating the guess and its feedback to add to the game board
 function createNewRow(guess, feedback) {
+    //create a row with the class name "mmRow"
     let mmRow = document.createElement("div");
     mmRow.className = "mmRow";
+    //create a container for the guess indicators
     let guessContainer = document.createElement("div");
     guessContainer.className = "guess";
+    //create a container for the feedback indicators
     let feedbackContainer = document.createElement("div");
     feedbackContainer.className = "feedback";
+    //put the guess container in the row
     mmRow.appendChild(guessContainer);
+    //put the feedback container in the row
     mmRow.appendChild(feedbackContainer);
+    //create an indicator for each color in the guess array
     for(let color of guess) {
         let indicator = document.createElement("div");
         indicator.className = color + " indicator";
+        //and put it in the guess container
         guessContainer.appendChild(indicator);
     }
+    //create an indicator for each color in the feedback array
     for(let color of feedback) {
         let indicator = document.createElement("div");
         indicator.className = color + " indicator";
+        //and put it in the feedback container
         feedbackContainer.appendChild(indicator);
     }
     return mmRow;
