36a56ab1a8bbac9ee317ea8f68201f00b6a9440f
michaeleiseman <eiseman@att.net>
2023-06-16 22:38:47 -0400

    add Braille Converter

---
+++ Day10Exercises/Day10_Exercise05_BrailleConverter/BrailleConverter/BrailleConverter.js
@@ -0,0 +1,40 @@
+brailleMap = {
+    'a': '⠁',
+    'b': '⠃',
+    'c': '⠉',
+    'd': '⠙',
+    'e': '⠑',
+    'f': '⠋',
+    'g': '⠛',
+    'h': '⠓',
+    'i': '⠊',
+    'j': '⠚',
+    'k': '⠅',
+    'l': '⠇',
+    'm': '⠍',
+    'n': '⠝',
+    'o': '⠕',
+    'p': '⠏',
+    'q': '⠟',
+    'r': '⠗',
+    's': '⠎',
+    't': '⠞',
+    'u': '⠥',
+    'v': '⠧',
+    'w': '⠺',
+    'x': '⠭',
+    'y': '⠽',
+    'z': '⠵',
+    ' ': '⠀'
+  };
+let input = document.getElementsByTagName("input")[0];
+window.addEventListener('keydown', convertToBraille, false);
+function convertToBraille() {
+    /****** WRITE YOUR CODE HERE */
+    //This code should:
+      //1. Access the current text in the input element
+      //2. Split that text up into an array of characters
+      //3. Map each character to its corresponding Braille symbol which can be found using BrailleMap[character]
+      //4. Join the mapped characters together in a single string
+      //5. Make the textContent of the output div equal to this single string.
+}
---
+++ Day10Exercises/Day10_Exercise05_BrailleConverter/BrailleConverterSolution/BrailleConverter.js
@@ -0,0 +1,34 @@
+brailleMap = {
+    'a': '⠁',
+    'b': '⠃',
+    'c': '⠉',
+    'd': '⠙',
+    'e': '⠑',
+    'f': '⠋',
+    'g': '⠛',
+    'h': '⠓',
+    'i': '⠊',
+    'j': '⠚',
+    'k': '⠅',
+    'l': '⠇',
+    'm': '⠍',
+    'n': '⠝',
+    'o': '⠕',
+    'p': '⠏',
+    'q': '⠟',
+    'r': '⠗',
+    's': '⠎',
+    't': '⠞',
+    'u': '⠥',
+    'v': '⠧',
+    'w': '⠺',
+    'x': '⠭',
+    'y': '⠽',
+    'z': '⠵',
+    ' ': '⠀'
+  };
+let input = document.getElementsByTagName("input")[0];
+window.addEventListener('keydown', convertToBraille, false);
+function convertToBraille() {
+    document.getElementById("output").textContent = input.value.split("").map(letter => brailleMap[letter.toLowerCase()]).join("");
+}
