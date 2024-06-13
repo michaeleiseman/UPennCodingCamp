962b68345aaca1e656a52f3dabacf5c0364b9ac0
Michael Eiseman <mrmike@algebraybyhand.com>
2024-06-13 14:44:25 -0400

    turn box blue/red edit

--- Day05Exercises/Day05_Exercise04_EventListeners/EventListenersEasier/EventListeners.js
+++ Day05Exercises/Day05_Exercise04_EventListeners/EventListenersEasier/EventListeners.js
@@ -1,8 +1,8 @@
 let box = document.getElementById("box");
-/* Attach an event listener to the red button that will turn the box red
+/* Attach an event listener to the red button that will call a function to turn the box red
 when the user clicks on the red button. */
 
-/* Attach an event listener to the blue button that will turn the box blue
+/* Attach an event listener to the blue button that will call a function to turn the box blue
 when the user clicks on the blue button. */
 
 //create a function that turns the box red
