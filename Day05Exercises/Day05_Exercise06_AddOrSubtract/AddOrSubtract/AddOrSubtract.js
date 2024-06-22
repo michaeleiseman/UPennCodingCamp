271143cb1ad89f310c2b86fb425f66d128ceb6d8
michaeleiseman <eiseman@att.net>
2024-06-22 15:32:13 -0400

    correct AddOrSubtractHarder

--- Day05Exercises/Day05_Exercise06_AddOrSubtract/AddOrSubtract/AddOrSubtract.js
+++
@@ -1,5 +0,0 @@
-/* When the user clicks on the "=" and the checkbox is NOT checked,
-the calculator should display the SUM of the two numbers in the
-input fields.  When the user clicks on the "=" and the checkbox IS checked,
-the calculator should display the SUBTRACTION of the value in the second
-input field from the value of the first input field."*/
